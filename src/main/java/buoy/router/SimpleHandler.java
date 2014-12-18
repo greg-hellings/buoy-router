12cac07a6f233b179962305a7ae5cf9dc004a93f
Gregory Hellings <greg.hellings@gmail.com>
2014-12-18 16:23:53 -0600

    Expanding behavior of invocation reflection

--- src/main/java/buoy/router/SimpleHandler.java
+++ src/main/java/buoy/router/SimpleHandler.java
@@ -6,7 +6,12 @@
 package buoy.router;
 
 import buoy.router.exceptions.InvalidHandlerException;
+import buoy.router.utils.Argument;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +23,7 @@ public class SimpleHandler implements Handler {
 
 	private Class clazz;
 	private Method method;
+	private List<Constructor> arguments;
 	private static Logger log = Logger.getLogger(SimpleHandler.class.getName());
 
 	public SimpleHandler(String classname, String methodname) throws InvalidHandlerException {
@@ -38,5 +44,23 @@ public class SimpleHandler implements Handler {
 		if (this.method == null) {
 			throw new InvalidHandlerException("No method of " + methodname + " found on class " + classname);
 		}
+	}
+
+	public SimpleHandler(Class clazz, Method method) {
+		this.clazz = clazz;
+		this.method = method;
+	}
+
+	private void extractArguments() {
+		Parameter[] parameters = this.method.getParameters();
+		this.arguments = new ArrayList<>(parameters.length);
+		for (Parameter parameter : parameters) {
+			this.arguments.add(new Argument(parameter.getName(), parameter.getType()));
+		}
+	}
+
+	@Override
+	public void handleRequest() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 }
---
+++ src/main/java/buoy/router/utils/Argument.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buoy.router.utils;
+
+import buoy.router.Invocation;
+import com.google.common.collect.ImmutableMap;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author greg
+ */
+public class Argument {
+
+	/**
+	 *
+	 */
+	public static ImmutableMap<Class, Class> primitives
+			= ImmutableMap.<Class, Class>builder()
+			.put(byte.class, Byte.class)
+			.put(short.class, Short.class)
+			.put(int.class, Integer.class)
+			.put(long.class, Long.class)
+			.put(float.class, Float.class)
+			.put(double.class, Double.class)
+			.put(boolean.class, Boolean.class)
+			.put(char.class, Character.class)
+			.build();
+
+	private String name;
+	private Constructor type;
+	private static final Logger log = Logger.getLogger(Argument.class.getName());
+
+	public Argument(String name, Class type) throws NoSuchMethodException {
+		this.name = name;
+		try {
+			this.type = type.getConstructor(String.class);
+		} catch (NoSuchMethodException ex) {
+			log.log(Level.SEVERE, "No string constructor found", ex);
+			throw ex;
+		} catch (SecurityException ex) {
+			log.log(Level.SEVERE, "Constructor found, but inaccessible", ex);
+			throw ex;
+		}
+	}
+
+	public Object getType(Invocation invocation) {
+		Object object = null;
+		if (invocation.hasKey(this.name)) {
+			try {
+				object = this.type.newInstance(invocation.getValue(this.name));
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+				log.log(Level.SEVERE, "Error intantiating argument from Invocation", ex);
+			}
+		}
+
+		return object;
+	}
+}
