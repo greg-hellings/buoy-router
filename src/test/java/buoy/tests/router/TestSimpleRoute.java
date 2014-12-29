976b7c493363ad800d45ccc9a053efc13bc54215
Greg Hellings <greg.hellings@gmail.com>
2014-12-28 19:17:18 -0600

    Proper renaming of the test packages.

--- src/test/java/buoy/tests/router/TestSimpleRoute.java
+++ src/test/java/buoy/tests/router/TestSimpleRoute.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bouy.tests.router;
+package buoy.tests.router;
 
 import buoy.router.SimpleRoute;
 import buoy.router.http.Verb;
